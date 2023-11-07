1394892bc2809d21cacba1755c0e0348d91ee28f
andrewsharanda <azzimandias@gmail.com>
2023-11-07 21:05:19 +0300

    welcome view

---
+++ src/components/UI/SignButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="sign-btn">
+    <slot/>
+  </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped lang="scss">
+  @import "../../assets/styles/global.scss";
+  .sign-btn {
+    width: 150px;
+    height: 60px;
+    font-size: 16px;
+    cursor: pointer;
+    &:active {
+      opacity: .8;
+    }
+  }
+  .sign-in {
+    color: $gold;
+    background-color: transparent;
+    border: 1px solid $gold;
+  }
+  .sign-up {
+    color: $textColorActive;
+    background-color: $gold;
+    border: 1px solid $gold;
+  }
+</style>
--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,13 +1,61 @@
 <template>
-  <div>
-    <router-link to="/workspace">К работе!</router-link>
+  <div class="skin">
+    <div style="display: none;"><router-link to="/workspace">К работе!</router-link></div>
+    <div class="top-block"></div>
+    <div class="header">
+      <div class="logo-wrapper">
+        <ApplicationName/>
+      </div>
+      <div class="btns-wrapper">
+        <SignButton :class="'sign-in'">Sign in</SignButton>
+        <SignButton :class="'sign-up'">Sign up</SignButton>
+      </div>
+    </div>
+    <div class="main-block">
+
+    </div>
   </div>
 </template>
 
 <script setup>
-
+  import ApplicationName from '@/components/UI/ApplicationName.vue';
+  import SignButton from '@/components/UI/SignButton.vue';
 </script>
 
 <style lang="scss" scoped>
+  @import "../assets/styles/global.scss";
+  .skin {
+    width: 100%;
+    height: 100%;
+    background-color: $tasker;
+  }
+  .top-block {
+    width: 100%;
+    height: 40px;
+    background-color: $personal;
+  }
+  .header {
+    width: 100%;
+    height: 90px;
+    padding: 15px 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .logo-wrapper {
+    width: 200px;
+    height: 100%;
+  }
+  .btns-wrapper {
+    width: 350px;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .main-block {
+    width: 100%;
+    height: calc(100vh - 130px);
 
+  }
 </style>
