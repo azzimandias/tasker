1ed468e1e2ee1df87486abf8d981543046f6275d
Andrew Sharanda <35540190+azzimandias@users.noreply.github.com>
2024-11-21 15:34:00 +0300

    for laravel sanctum

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   //publicPath: '/tasker/'
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://taskerlaravelapi.ru'
+      },
+    },
+  },
 })
