2b6a8f3ea7a1a71c597a8ffddebbfc35135a280f
Andrew Sharanda <35540190+azzimandias@users.noreply.github.com>
2024-11-21 12:07:22 +0300

    for laravel sanctum

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
 export const HTTP = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
     headers: {
