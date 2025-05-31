45b0f7102de1b760982759f795a799bffe1cfb5c
Andrew Sharanda <35540190+azzimandias@users.noreply.github.com>
2025-05-31 20:27:29 +0300

    socket fix

--- src/plugins/socket.js
+++ src/plugins/socket.js
@@ -3,6 +3,8 @@ import { io } from 'socket.io-client';
 const socket = io( process.env.VUE_APP_SOCKET_URL, {
     autoConnect: false,
     reconnection: true,
+    reconnectionAttempts: 5,
+    reconnectionDelay: 1000,
     transports: ['websocket'],
     withCredentials: true,
     rejectUnauthorized: false
